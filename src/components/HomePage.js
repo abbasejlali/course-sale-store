fb3aedeadabe7c637501818e3e9920d7c7c26b84
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-22 01:37:02 -0800

    add Banner.js to HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // components
 import Navbar from "./Navbar";
+import Banner from "./Banner";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 };
