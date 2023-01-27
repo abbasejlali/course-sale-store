fc58f75dd683a752586d2724ee1b72a5fdd417ac
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-26 23:42:45 -0800

    add footer.js and footer.module.css to project

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// styles
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            
+        </footer>
+    );
+};
+
+export default Footer;
