c82115838a3911ff63bad1cfc61ddf8988d49593
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-28 09:52:37 -0800

    styling positon sticky to navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,40 +30,42 @@ const Navbar = () => {
 
   return (
     <>
-      <section className={styles.main}>
-        <div className={styles.menu_icon_mobile} onClick={clickHandler}>
-          <FaBars />
-        </div>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
-        <nav className={styles.menu_nav}>
-          <ul className={styles.menu_ul}>
-            <li>
-              <Link to="/">Home Page</Link>
-            </li>
-            <li>
-              <Link to="/courses">Courses</Link>
-            </li>
-            <li>
-              <Link to="/comments">Comments</Link>
-            </li>
-            <li>
-              <Link to="/about-us">About Us</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.login_cart}>
-          <div className={styles.login}>
-            <span>Login</span>
-            <span>
-              <FaSignInAlt />
-            </span>
+      <section className={styles.section_main}>
+        <div className={styles.main}>
+          <div className={styles.menu_icon_mobile} onClick={clickHandler}>
+            <FaBars />
           </div>
-          <div className={styles.cart}>
-            <span>
-              <FaShoppingBasket />
-            </span>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <nav className={styles.menu_nav}>
+            <ul className={styles.menu_ul}>
+              <li>
+                <Link to="/">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/courses">Courses</Link>
+              </li>
+              <li>
+                <Link to="/comments">Comments</Link>
+              </li>
+              <li>
+                <Link to="/about-us">About Us</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.login_cart}>
+            <div className={styles.login}>
+              <span>Login</span>
+              <span>
+                <FaSignInAlt />
+              </span>
+            </div>
+            <div className={styles.cart}>
+              <span>
+                <FaShoppingBasket />
+              </span>
+            </div>
           </div>
         </div>
       </section>
