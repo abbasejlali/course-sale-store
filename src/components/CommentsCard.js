10f3b2ebf46c8e8ce0534fde76438358ece52100
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-29 08:21:03 -0800

    add jsx to commentscard.js

---
+++ src/components/CommentsCard.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// icons
+import { FaUserAlt } from "react-icons/fa";
+
+// video
+import video from "../video/kian-baban.mov";
+
+// styles
+import styles from "./CommentsCard.module.css";
+
+const CommentsCard = ({ data_comments }) => {
+  return (
+    <div className={styles.comment_main}>
+      <div className={styles.comment_text}>
+        <div className={styles.comment_titr}>
+          <FaUserAlt />
+          <div>
+            <h5>{data_comments.student_name}</h5>
+            <span>{data_comments.course_name}</span>
+          </div>
+        </div>
+        <div>
+          <p>{data_comments.comment}</p>
+        </div>
+      </div>
+      <div className={styles.comment_video}>
+        <video controls width="100%" height="100%">
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default CommentsCard;
--- src/components/CommentsCards.js
+++ src/components/CommentsCards.js
@@ -3,13 +3,16 @@ import React, { useContext } from "react";
 // contetx
 import { CommentsContext } from "../Context/CommentsContextProvider";
 
+// components
+import CommentsCard from "./CommentsCard";
+
 const CommentsCards = () => {
   const data = useContext(CommentsContext);
 
   return (
     <div>
       {data.map((item) => (
-        <p key={item.id}>{item.id}</p>
+        <CommentsCard key={item.id} data_comments={item} />
       ))}
     </div>
   );
