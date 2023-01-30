d6d35f701d10b8bb1e199ef69b7e5472d581057b
abbas ejlali <abbas.ejlali18@gmail.com>
2023-01-30 09:03:02 -0800

    styleing to loginbox.js

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+// styles
+import styles from "./LoginBox.module.css";
 
 const LoginBox = () => {
-  return <div>test</div>;
+  return (
+    <div className={styles.main}>
+      <h3>Register/login in Abbas Ejlali site</h3>
+      <button>
+        <span>Register or Login</span>
+        <FaSignInAlt />{" "}
+      </button>
+    </div>
+  );
 };
 
 export default LoginBox;
