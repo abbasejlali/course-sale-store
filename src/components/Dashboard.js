37c6f95024a9e1f66fda92d2173b260b5842809a
abbas ejlali <abbas.ejlali18@gmail.com>
2023-02-06 07:28:16 -0800

    styleing dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,11 +29,11 @@ const Dashboard = () => {
   return (
     <>
       <div className={styles.dashboard_main}>
-        <div className={styles.dashboard_main_up}>
+        {/* <div className={styles.dashboard_main_up}>
           <img src={user.photoURL} alt="photoprofile" />
           <h5>{user.bc.displayName}</h5>
           <span>{user.bc.email}</span>
-        </div>
+        </div> */}
         <ul className={styles.dashboard_main_bottom}>
           <li>
             <span>
@@ -61,17 +61,20 @@ const Dashboard = () => {
           </li>
         </ul>
       </div>
-      <div className={styles.dashboard_useraccont}>
-        <h4>Abbas Ejlali</h4>
-        <span>
-          Do you want to be a programmer? You have come to the right place{" "}
-          <FaRegHeart />
-        </span>
-        <p>
-          In the <span style={{ color: "rgb(126, 87, 194) !important" }}></span>
-          My Courses section, you can see all the courses you attended and how
-          to access them.
-        </p>
+      <div className={styles.dashboard_info}>
+        <div className={styles.dashboard_useraccont}>
+          <h4>Abbas Ejlali</h4>
+          <span>
+            Do you want to be a programmer? You have come to the right place{" "}
+            <FaRegHeart />
+          </span>
+          <p>
+            In the{" "}
+            <span style={{ color: "rgb(126, 87, 194) !important" }}></span>
+            My Courses section, you can see all the courses you attended and how
+            to access them.
+          </p>
+        </div>
       </div>
     </>
   );
