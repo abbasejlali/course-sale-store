ad29c4fc2971ce6818e624bc19fd099642bd65a4
abbas ejlali <abbas.ejlali18@gmail.com>
2023-02-08 08:30:00 -0800

    betting to login.js

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import firebase from "firebase/app";
 import { auth } from "./firebase";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // icons
 import { FaSignInAlt } from "react-icons/fa";
@@ -8,18 +11,32 @@ import { FaSignInAlt } from "react-icons/fa";
 // styles
 import styles from "./LoginBox.module.css";
 
+// context
+import { UserContext } from "../Context/UserContextProvider";
+
 const LoginBox = () => {
+  const user = useContext(UserContext);
+
   return (
     <div className={styles.main}>
-      <h3>Register/login in Abbas Ejlali site</h3>
-      <button
-        onClick={async () =>
-          await auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-        }
-      >
-        <span>Register or Login</span>
-        <FaSignInAlt />{" "}
-      </button>
+      {user ? (
+        <h3>You are already logged in</h3>
+      ) : (
+        <>
+          {" "}
+          <h3>Register/login in Abbas Ejlali site</h3>
+          <button
+            onClick={async () =>
+              await auth.signInWithRedirect(
+                new firebase.auth.GoogleAuthProvider()
+              )
+            }
+          >
+            <span>Register or Login</span>
+            <FaSignInAlt />{" "}
+          </button>
+        </>
+      )}
     </div>
   );
 };
