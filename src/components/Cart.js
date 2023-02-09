9f45032d668e66d17eab95f13def66411f6119fd
abbas ejlali <abbas.ejlali18@gmail.com>
2023-02-09 08:22:37 -0800

    add cart components to project

---
+++ src/components/Cart.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Cart = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Cart;
