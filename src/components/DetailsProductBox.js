391a96bce631d992fafb36ad0cb42a17053f50bc
abbas ejlali <abbas.ejlali18@gmail.com>
2023-02-14 10:50:18 -0800

    styling and responsive to detailsproductbox.js

--- src/components/DetailsProductBox.js
+++ src/components/DetailsProductBox.js
@@ -30,43 +30,45 @@ const DetailsProductBox = () => {
 
   return (
     <>
-      <div className={styles.product_box}>
-        <div className={styles.product_details}>
-          <img src={github} alt="product_img" />
-          <h1>{title}</h1>
-          <p>{discribtion}</p>
-          <div className={styles.btn_details}>
-            <button>Buy Course</button>
-            <span>{price} $</span>
+      <div className={styles.product_main}>
+        <div className={styles.product_box}>
+          <div className={styles.product_details}>
+            <img src={github} alt="product_img" />
+            <h1>{title}</h1>
+            <p>{discribtion}</p>
+            <div className={styles.btn_details}>
+              <button>Buy Course</button>
+              <span>{price} $</span>
+            </div>
           </div>
-        </div>
-        <div className={styles.product_video}>
-          <video
-            controls
-            poster="https://botostart.org/images/posters/javascript.png"
-            width="100%"
-          >
-            <source
-              src="https://botostart.org/courses/other/javascript/videos/1-%20introduction.mp4"
-              type="video/mp4"
-            />
-          </video>
-          <div className={styles.product_icons}>
-            <div>
-              <FaUserGraduate />
-              <span>{number}</span>
-            </div>
-            <div>
-              <FaUserClock />
-              <span>{number_sessions}</span>
-            </div>
-            <div>
-              <FaClock />
-              <span>{clock}</span>
-            </div>
-            <div>
-              <FaHeadphonesAlt />
-              <span>Permanent Support</span>
+          <div className={styles.product_video}>
+            <video
+              controls
+              poster="https://botostart.org/images/posters/javascript.png"
+              width="100%"
+            >
+              <source
+                src="https://botostart.org/courses/other/javascript/videos/1-%20introduction.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div className={styles.product_icons}>
+              <div>
+                <FaUserGraduate />
+                <span>{number}</span>
+              </div>
+              <div>
+                <FaUserClock />
+                <span>{number_sessions}</span>
+              </div>
+              <div>
+                <FaClock />
+                <span>{clock}</span>
+              </div>
+              <div>
+                <FaHeadphonesAlt />
+                <span>24H Support</span>
+              </div>
             </div>
           </div>
         </div>
